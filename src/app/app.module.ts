b47d734bff5732b5d2e866985d4fb45221ee81c2
Harikrishna Daxini <HarikrishnaD@sfwltd.co.uk>
2021-08-05 09:56:05 +0530

    Committed the changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ClistComponent } from './clist/clist.component';
+import { HlistComponent } from './hlist/hlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClistComponent,
+    HlistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'c-list', component: ClistComponent},
+      {path: 'heroes-list', component: HlistComponent},
+      {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+    ]),
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
